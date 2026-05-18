f5cffa0768641c9296d4717b2dcbe623a8fcedb6
Aanchalsharma923 <aanchal2361@gmail.com>
2026-05-18 19:26:24 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,93 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
 
-struct Node {
-    int data;
-    struct Node *left, *right;
-    bool lthread, rthread;
+struct node{
+    int data ;
+    struct node *left;
+    struct node *right;
+    int rthread;
 };
 
-struct Node *root = NULL;
+struct node *root= NULL;
 
-void insertTBT(int key) {
-    struct Node *ptr = root;
-    struct Node *parent = NULL;
+struct node* createnode(int data){
+    struct node* newnode= (struct node*)malloc(sizeof(struct node));
+    newnode->data=data;
+    newnode->left = newnode->right= NULL;
+    newnode->rthread=1;
+    return newnode;
+}
 
-    while (ptr != NULL) {
-        if (key == ptr->data)
-            return;
+void insert(int key)
+{
+    struct node *ptr=root;
+    struct node *parent=NULL;
 
-        parent = ptr;
-
-        if (key < ptr->data) {
-            if (ptr->lthread == 0)
-                ptr = ptr->left;
-            else
-                break;
-        } else {
-            if (ptr->rthread == 0)
-                ptr = ptr->right;
-            else
-                break;
+    while(ptr!=NULL)
+    {
+        parent=ptr;
+        if(key<ptr->data)
+        ptr=ptr->left;
+        else{
+            if(ptr->rthread==0)
+            ptr=ptr->right;
+            else break;
+            
         }
     }
+    struct node *newnode= createnode(key);
+    if(parent==NULL)
+    {
+        root=newnode;
+    }
+    else if(key<parent->data)
+    {
+        parent->left=newnode;
+        newnode->right=parent;
+    }
+    else{
+        newnode->right=parent->right;
+        parent->right=newnode;
+        parent->rthread=0;
+         
+    }
 
-    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    newNode->data = key;
-    newNode->lthread = 1;
-    newNode->rthread = 1;
-
-    if (parent == NULL) {
-        root = newNode;
-        newNode->left = NULL;
-        newNode->right = NULL;
-    }
-    else if (key < parent->data) {
-        newNode->left = parent->left;
-        newNode->right = parent;
-        parent->lthread = 0;
-        parent->left = newNode;
-    }
-    else {
-        newNode->left = parent;
-        newNode->right = parent->right;
-        parent->rthread = 0;
-        parent->right = newNode;
-    }
+        
 }
 
-struct Node* leftMost(struct Node *n) {
-    while (n != NULL && n->lthread == 0)
-        n = n->left;
-    return n;
+struct node *leftmost(struct node *node){
+    while(node!=NULL && node->left!=NULL)
+    node= node->left;
+    return node;
 }
 
-void inorderTBT() {
-    struct Node *cur = leftMost(root);
-
-    while (cur != NULL) {
+void inorder(){
+    struct node *cur=leftmost(root);
+    while(cur!=NULL)
+    {
         printf("%d ", cur->data);
-
-        if (cur->rthread == 1)
-            cur = cur->right;
+        if(cur->rthread==1)
+        cur=cur->right;
         else
-            cur = leftMost(cur->right);
-    }
+        cur=leftmost(cur->right);
+    }       
 }
+void preorder()
+{
+    struct node *cur = root;
 
-void preorderTBT() {
-    struct Node *cur = root;
-
-    while (cur != NULL) {
+    while (cur != NULL)
+    {
+        // Visit the node
         printf("%d ", cur->data);
 
-        if (cur->lthread == 0)
+        // If left child exists, go left
+        if (cur->left != NULL)
+        {
             cur = cur->left;
-        else if (cur->rthread == 0)
-            cur = cur->right;
-        else {
+        }
+        else
+        {
+            // Follow threads until a real right child is found
             while (cur != NULL && cur->rthread == 1)
                 cur = cur->right;
 
@@ -97,72 +98,18 @@ void preorderTBT() {
     }
 }
 
-void inorderRecursive(struct Node *node) {
-    if (node == NULL)
-        return;
 
-    if (node->lthread == 0)
-        inorderRecursive(node->left);
-
-    printf("%d ", node->data);
-
-    if (node->rthread == 0)
-        inorderRecursive(node->right);
-}
-
-void preorderRecursive(struct Node *node) {
-    if (node == NULL)
-        return;
-
-    printf("%d ", node->data);
-
-    if (node->lthread == 0)
-        preorderRecursive(node->left);
-
-    if (node->rthread == 0)
-        preorderRecursive(node->right);
-}
-
-
-int main() {
-    int n;
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i++) {
-        int val = rand() % 1000;
-        insertTBT(val);
-    }
-
-    clock_t start, end;
-
-    printf("\nRecursive Inorder Traversal:\n");
-    start = clock();
-    inorderRecursive(root);
-    end = clock();
-    printf("\nTime (Recursive Inorder): %lf seconds\n",
-           (double)(end - start) / CLOCKS_PER_SEC);
-
-    printf("\nStackless Inorder Traversal:\n");
-    start = clock();
-    inorderTBT();
-    end = clock();
-    printf("\nTime (Non-Recursive Inorder): %lf seconds\n",
-           (double)(end - start) / CLOCKS_PER_SEC);
-
-    printf("\n\nRecursive Preorder Traversal:\n");
-    start = clock();
-    preorderRecursive(root);
-    end = clock();
-    printf("\nTime (Recursive Preorder): %lf seconds\n",
-           (double)(end - start) / CLOCKS_PER_SEC);
-
-    printf("\nStackless Preorder Traversal:\n");
-    start = clock();
-    preorderTBT();
-    end = clock();
-    printf("\nTime (Non-Recursive Preorder): %lf seconds\n",
-           (double)(end - start) / CLOCKS_PER_SEC);
-
-    return 0;
+void main(){
+    printf("no.of nodes to be inserted: ");
+    int n; scanf("%d", &n);
+    printf("enter the nodes: ");    
+    for(int i=0;i<n;i++){
+        int data; scanf("%d", &data);
+        insert(data);
+    }  
+    printf("inorder traversal: ");  
+    inorder();                      
+    printf("\npreorder traversal: ");  
+    preorder(); 
+    return;
 }
